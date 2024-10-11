65cb9457e8861fa1b2d7d4954ccc36c6c00f51a3
Ivan Mysovskii <vano641@mail.ru>
2024-10-11 13:07:05 +0300

    Задача2 Инкапсуляция - Конструкторы, get/set методы

--- task1_Product.java
+++ task1_Product.java
@@ -2,7 +2,31 @@
  * task1_Product 
  */
 public class task1_Product { // Класс продукт
-// поля класса:
-    String name; // название продукта
-    int price; // цена продукта
+// конструктор из полей класса:
+    public task1_Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+// поля класса приватные:
+    private String name; // название продукта
+    private int price; // цена продукта
+
+// метод getName возвращает Название
+    public String getName() {
+        return this.name;
+    }
+// метод setName присваивает Название (Присваивает "значение" полю name в Классе task1_Product )
+    public void setName(String name) {
+        this.name = name;
+    }
+// возвращает Цену продукта
+    public int getPrice() {
+        return this.price;
+    }
+// Присваивает Цену продукту 
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
 }
