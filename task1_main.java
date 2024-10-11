ba2eb5bfb078b6146ef9f7a9dc314f6de1fa5c08
Ivan Mysovskii <vano641@mail.ru>
2024-10-11 11:17:30 +0300

    Задача 1 - Абстракция

---
+++ task1_main.java
@@ -0,0 +1,14 @@
+/**
+ * task1_main 
+ * 1) Реальизуйте класс "Product" содержащий данный о товаре и
+ *    класс VendingMachine содержащий в себе методы:
+ *      initProducts(List<Product>) - сохраняющий в себе список исходных продуктов
+ *      getProduct(String name) - который возвращает товар по его названию
+ */
+public class task1_main {
+    public static void main(String[] args) {
+    
+        
+    }
+    
+}
