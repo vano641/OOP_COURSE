f4cbae155b04c70f3de0a58f37f310ff9301e9db
Ivan Mysovskii <vano641@mail.ru>
2024-10-11 12:06:35 +0300

    Условия Задачи2 - Инкапсуляция

--- task1_main.java
+++ task1_main.java
@@ -4,6 +4,8 @@
  *    класс VendingMachine содержащий в себе методы:
  *      initProducts(List<Product>) - сохраняющий в себе список исходных продуктов
  *      getProduct(String name) - который возвращает товар по его названию
+ * 2) Реализуйте конструкторы, get/set методы, постройте логику VendingMachine на основе
+ * кода сделанного в Пункте 1.
  */
 public class task1_main {
     public static void main(String[] args) {
