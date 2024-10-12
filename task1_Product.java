5b0e62916bb0b1a652151370743e89bd4cc5f442
Ivan Mysovskii <vano641@mail.ru>
2024-10-12 20:38:21 +0300

    Добавил в Класс метод Форматированного вывода и Переопределил метод toString для наглядности, выбрать можно любой метод

--- task1_Product.java
+++ task1_Product.java
@@ -1,8 +1,18 @@
 /**
- * task1_Product 
+ * это класс task1_Product (Товар)
  */
-public class task1_Product { // Класс продукт
-// поля класса:
-    String name; // название продукта
-    int price; // цена продукта
+public class task1_Product {
+    
+    String name;
+    int price;
+
+// метод форматированного вывода
+    public String getInfo(){
+        return String.format("name: %s => price: %d", name, price);
+    }
+// переопределение метода toString
+    @Override
+    public String toString() {
+        return getInfo();
+    }
 }
