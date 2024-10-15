ede59d7e1e98f0e7caa89e8e3e1b70d07d289c0e
Ivan Mysovskii <vano641@mail.ru>
2024-10-15 19:32:44 +0300

    Задача3: класс Продукты - Абстрактный

--- task1_Product.java
+++ task1_Product.java
@@ -1,29 +1,35 @@
-public abstract class task1_Product { // Класс продукт Абстрактный
-// конструктор из полей класса:
-    public task1_Product(String name, int price) {
-        this.name = name;
-        this.price = price;
+/**
+ * Класс Продукт Абстрактный
+ */
+public abstract class task1_Product {
+    // конструктор из полей класса:
+        public task1_Product(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
+       
+        protected String name; // название продукта
+        protected int price; // цена продукта
+    
+    // метод getName возвращает Название
+        public String getName() {
+            return this.name;
+        }
+    // метод setName присваивает Название (Присваивает "значение" полю name в Классе task1_Product )
+        public void setName(String name) {
+            this.name = name;
+        }
+    // возвращает Цену продукта
+        public int getPrice() {
+            return this.price;
+        }
+    // Присваивает Цену продукту 
+        public void setPrice(int price) {
+            this.price = price;
+        }
+    // переопределим toString
+        @Override
+        public String toString() {
+            return String.format("name: %s => price: %d", name, price);
+        }   
     }
-
-// поля класса приватные:
-    private String name; // название продукта
-    private int price; // цена продукта
-
-// метод getName возвращает Название
-    public String getName() {
-        return this.name;
-    }
-// метод setName присваивает Название (Присваивает "значение" полю name в Классе task1_Product )
-    public void setName(String name) {
-        this.name = name;
-    }
-// возвращает Цену продукта
-    public int getPrice() {
-        return this.price;
-    }
-// Присваивает Цену продукту 
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-}
